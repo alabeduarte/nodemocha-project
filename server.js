068359dcec595c054632bc0b3c219a781826baa5
Alabê Duarte <alabeduarte@gmail.com>
2012-09-18 00:19:37 -0300

    creating server listening on development environment

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express"),
   mongoose = require("mongoose"),
+  config = require("./config")
   app = express()
 ;
 
-app.configure(function()  {
-  /* ExpressJs Middlewares */
-  // Simulate another http verbs (DELETE, PUT, etc)
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-});
+/* ExpressJs Middlewares */
+// Simulate another http verbs (DELETE, PUT, etc)
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(express.logger());
+app.use(express.static(__dirname + "/public"));
+
+app.listen(config.serverPort);
+console.log("=> Listening in %s on http://0.0.0.0:%s", config.env, config.serverPort);
